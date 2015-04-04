09b68a6d552742448c6408362c41f71356f981fe
GlitchEnzo <mccarthp@gmail.com>
2015-04-03 21:57:40 -0700

    Added Song and Album classes
    They are ported directly from old JavaScript code, so they are missing
    some references that will be added later.

---
+++ Album.ts
@@ -0,0 +1,56 @@
+﻿/**
+ * Represents an Album for an artist that holds a list of songs.
+ * @class Represents an Album
+ */
+class Album
+{
+    /*
+    * The name of this album.
+    */
+    name: string = "Unknown";
+
+    /*
+    * The name of the artist who made this album.
+    */
+    artist: string = "Unknown";
+
+    /*
+    * The list of songs associated with this album.
+    */
+    songs: Song[] = [];
+
+    CreateListItem() 
+    {
+        var listItem = document.createElement("li");
+
+        listItem.innerHTML = "<div><img src=\"controls/folder.png\"/> " + this.name + " [" + this.artist + "] (" + this.songs.length + " songs)</div>";
+
+        var onclick = function () { this.Open(true); };
+
+        // we must bind the click method to this Album object in order to make the "this" object in the handler point to this Album object
+        listItem.onclick = onclick.bind(this);
+
+        return listItem;
+    }
+
+
+    Open(pushState: boolean)
+    {
+        if (pushState)
+        {
+            // push the state into the browser history so can navigate back to it
+            // pushState(stateObject, title, url)
+            history.pushState({ album: this.name }, "Unused", null);
+        }
+        currentFolder.innerHTML = this.name;
+
+        thelist.innerHTML = "";
+
+        for (var i = 0; i < this.songs.length; i++)
+        {
+            var song = this.songs[i];
+            var listItem = song.CreateListItem();
+            thelist.appendChild(listItem);
+        }
+    }
+} 
---
+++ Song.ts
@@ -0,0 +1,30 @@
+﻿class Song
+{
+    genre = "Unknown";
+    artist = "Unknown";
+    album = "Unknown";
+    track = "Unknown";
+    title = "Unknown";
+    path = "Unknown";
+
+    CreateListItem()
+    {
+        var listItem = document.createElement("li");
+
+        listItem.innerHTML = "<div><img src=\"controls/music.png\"/> " + this.artist + " - " + this.track + " - " + this.title + "</div>";
+
+        // store the path to the song on the list item itelf to let the player access it
+        listItem.setAttribute("data-path", this.path);
+
+        var onclick = function ()
+        {
+            // use global playlist object to queue up this song and all others at the same level
+            playlist.addToPlaylist(this);
+        };
+
+        // we must bind the click method to this Song object in order to make the "this" object in the handler point to this Song object
+        listItem.onclick = onclick.bind(this);
+
+        return listItem;
+    }
+} 
